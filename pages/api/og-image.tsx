da4b3fbe5b0149407a6e21c91112734eea01fb9f
herohoro <mtwd.office.25@gmail.com>
2023-05-29 00:39:14 +0900

    Fix multi-import_next/server

--- pages/api/og-image.tsx
+++ pages/api/og-image.tsx
@@ -1,5 +1,4 @@
-import { NextRequest } from 'next/server'
-import { ImageResponse } from 'next/server'
+import { NextRequest, ImageResponse } from 'next/server'
 import { NEXT_PUBLIC_SITE_TITLE } from '../../app/server-constants'
 import { Post } from '../../lib/notion/interfaces'
 
